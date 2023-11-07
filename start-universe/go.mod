fc0fa67de4be925c37be14202adb232dbc041ebc
deadprogram <ron@hybridgroup.com>
2023-11-07 18:23:32 +0100

    Initial commit
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ start-universe/go.mod
@@ -0,0 +1,12 @@
+module github.com/start_universe
+
+go 1.21
+
+toolchain go1.21.1
+
+require (
+	github.com/acifani/vita v1.1.1-0.20231106234643-e2e6e74c84dd
+	github.com/fermyon/spin/sdk/go/v2 v2.0.0
+)
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
---
+++ start-universe/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"net/http"
+
+	"github.com/acifani/vita/lib/game"
+	spinhttp "github.com/fermyon/spin/sdk/go/v2/http"
+	"github.com/fermyon/spin/sdk/go/v2/kv"
+)
+
+const (
+	width, height  uint32 = 32, 32
+	livePopulation        = 75
+	key                   = "universe"
+)
+
+func init() {
+	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			store, err := kv.OpenStore("default")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer store.Close()
+
+			key := generateKey()
+			universe := game.NewUniverse(height, width)
+			universe.Randomize(livePopulation)
+
+			data := make([]byte, height*width)
+			universe.Read(data)
+
+			if err := store.Set(key, data); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+		default:
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+}
+
+func main() {}
+
+func generateKey() string {
+	var result [32]byte
+	rand.Read(result[:])
+	encodedString := hex.EncodeToString(result[:])
+	return encodedString
+}
