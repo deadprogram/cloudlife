9571e5873cf02edb033c6058b71425fff8c65304
deadprogram <ron@hybridgroup.com>
2023-11-07 19:55:44 +0100

    universe: it is a RESTful Universe now
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- universe/main.go
+++ universe/main.go
@@ -17,15 +17,15 @@ const (
 
 func init() {
 	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		store, err := kv.OpenStore("default")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer store.Close()
+
 		switch r.Method {
 		case http.MethodPost:
-			store, err := kv.OpenStore("default")
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			defer store.Close()
-
 			key := generateKey()
 			universe := game.NewUniverse(height, width)
 			universe.Randomize(livePopulation)
@@ -40,6 +40,64 @@ func init() {
 
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte(key))
+
+		case http.MethodGet:
+			value, err := store.Get(r.URL.Path)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			universe := game.NewUniverse(height, width)
+			universe.Write(value)
+
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(universe.String()))
+
+		case http.MethodPut:
+			key := r.URL.Path
+			value, err := store.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			universe := game.NewUniverse(height, width)
+			universe.Write(value)
+			universe.Tick()
+
+			data := make([]byte, height*width)
+			universe.Read(data)
+
+			if err := store.Set(key, data); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(universe.String()))
+
+		case http.MethodDelete:
+			if err := store.Delete(r.URL.Path); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+
+		case http.MethodHead:
+			exists, err := store.Exists(r.URL.Path)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if exists {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			w.WriteHeader(http.StatusNotFound)
 
 		default:
 			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
