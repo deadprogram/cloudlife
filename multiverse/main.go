63fa193fa8749e07c3ced780cb2b1565cca971fc
deadprogram <ron@hybridgroup.com>
2023-11-12 10:25:46 +0100

    multiverse: move operations into separate functions for clarity
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- multiverse/main.go
+++ multiverse/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
 	spinhttp "github.com/fermyon/spin/sdk/go/v2/http"
@@ -25,26 +26,21 @@ func init() {
 		switch r.Method {
 		case http.MethodPost:
 			// create all of the universes
-			var universes []string
-
-			for i := 0; i < defaultCount*defaultCount; i++ {
-				r, err := spinhttp.Post("/universe", "application/text", nil)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
+			n := defaultCount
+			q := r.URL.Query()
+			if val := q.Get("n"); val != "" {
+				if c, err := strconv.Atoi(val); err == nil {
+					n = c
 				}
-				defer r.Body.Close()
-
-				if r.StatusCode != http.StatusOK {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-
-				body, err := io.ReadAll(r.Body)
-				universes = append(universes, strings.Trim(string(body), "\n\""))
 			}
 
-			if err := connectUniverses(universes); err != nil {
+			universes, err := createUniverses(n)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if err := connectUniverses(universes, n); err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
@@ -54,7 +50,7 @@ func init() {
 			w.Write([]byte("\n"))
 
 		case http.MethodGet:
-			// get list of the universes
+			// get keys to the universes
 			unis, err := store.GetKeys()
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -66,42 +62,21 @@ func init() {
 			w.Write([]byte("\n"))
 
 		case http.MethodPut:
-			// run a generation on all universes
+			// run tick on all universes
 			unis, err := store.GetKeys()
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
 
-			var universes []string
-			for _, v := range unis {
-				pth, _ := url.JoinPath("/universe", v)
-				u := &url.URL{
-					Path: pth,
-				}
-
-				req := http.Request{
-					Method: http.MethodPut,
-					URL:    u,
-				}
-				r, err := spinhttp.Send(&req)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-				defer r.Body.Close()
-
-				if r.StatusCode != http.StatusOK {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-
-				body, err := io.ReadAll(r.Body)
-				universes = append(universes, string(body))
+			result, err := multitick(unis)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 
 			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(strings.Join(universes, "")))
+			w.Write([]byte(strings.Join(result, "")))
 
 		case http.MethodDelete:
 			// delete all universes
@@ -111,26 +86,9 @@ func init() {
 				return
 			}
 
-			for _, v := range unis {
-				pth, _ := url.JoinPath("/universe", v)
-				u := &url.URL{
-					Path: pth,
-				}
-
-				req := http.Request{
-					Method: http.MethodDelete,
-					URL:    u,
-				}
-				r, err := spinhttp.Send(&req)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-
-				if r.StatusCode != http.StatusOK {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
+			if err := deleteUniverses(unis); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 
 			w.WriteHeader(http.StatusOK)
@@ -146,75 +104,83 @@ func init() {
 	})
 }
 
-func connectUniverses(multi []string) error {
-	number := defaultCount
+func createUniverses(n int) ([]string, error) {
+	var unis []string
+	for i := 0; i < n*n; i++ {
+		u, err := createUniverse()
+		if err != nil {
+			return unis, err
+		}
+
+		unis = append(unis, u)
+	}
+
+	return unis, nil
+}
+
+func createUniverse() (string, error) {
+	r, err := spinhttp.Post("/universe", "application/text", nil)
+	if err != nil {
+		return "", err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != http.StatusOK {
+		return "", errors.New("failed to create universe")
+	}
+
+	body, err := io.ReadAll(r.Body)
+	return strings.Trim(string(body), "\n\""), nil
+}
+
+func connectUniverses(multi []string, n int) error {
 	i := 0
-	for row := 0; row < number; row++ {
-		for col := 0; col < number; col++ {
+	for row := 0; row < n; row++ {
+		for col := 0; col < n; col++ {
+			var err error
+
 			switch {
 			// top right
-			case col == number-1 && row == number-1:
-				err := updateNeighbors(multi[i], "", multi[i-number], multi[i-1], "")
-				if err != nil {
-					return err
-				}
+			case col == n-1 && row == n-1:
+				err = updateNeighbors(multi[i], "", multi[i-n], multi[i-1], "")
 
 			// top left
-			case col == 0 && row == number-1:
-				err := updateNeighbors(multi[i], "", multi[i-number], "", multi[i+1])
-				if err != nil {
-					return err
-				}
+			case col == 0 && row == n-1:
+				err = updateNeighbors(multi[i], "", multi[i-n], "", multi[i+1])
 
 			// bottom right
-			case col == number-1 && row == 0:
-				err := updateNeighbors(multi[i], multi[i+number], "", multi[i-1], "")
-				if err != nil {
-					return err
-				}
+			case col == n-1 && row == 0:
+				err = updateNeighbors(multi[i], multi[i+n], "", multi[i-1], "")
 
 			// bottom left
 			case col == 0 && row == 0:
-				err := updateNeighbors(multi[i], multi[i+number], "", "", multi[i+1])
-				if err != nil {
-					return err
-				}
+				err = updateNeighbors(multi[i], multi[i+n], "", "", multi[i+1])
 
 			// first column
 			case col == 0:
-				err := updateNeighbors(multi[i], multi[i+number], multi[i-number], "", multi[i+1])
-				if err != nil {
-					return err
-				}
+				err = updateNeighbors(multi[i], multi[i+n], multi[i-n], "", multi[i+1])
 
 			// first row
 			case row == 0:
-				err := updateNeighbors(multi[i], multi[i+number], "", multi[i-1], multi[i+1])
-				if err != nil {
-					return err
-				}
+				err = updateNeighbors(multi[i], multi[i+n], "", multi[i-1], multi[i+1])
 
 			// last column
-			case col == number-1:
-				err := updateNeighbors(multi[i], multi[i+number], multi[i-number], multi[i-1], "")
-				if err != nil {
-					return err
-				}
+			case col == n-1:
+				err = updateNeighbors(multi[i], multi[i+n], multi[i-n], multi[i-1], "")
 
 			// last row
-			case row == number-1:
-				err := updateNeighbors(multi[i], "", multi[i-number], multi[i-1], multi[i+1])
-				if err != nil {
-					return err
-				}
+			case row == n-1:
+				err = updateNeighbors(multi[i], "", multi[i-n], multi[i-1], multi[i+1])
 
 			// anyplace else
 			default:
-				err := updateNeighbors(multi[i], multi[i+number], multi[i-number], multi[i-1], multi[i+1])
-				if err != nil {
-					return err
-				}
+				err = updateNeighbors(multi[i], multi[i+n], multi[i-n], multi[i-1], multi[i+1])
 			}
+
+			if err != nil {
+				return err
+			}
+
 			i++
 		}
 	}
@@ -240,6 +206,68 @@ func updateNeighbors(id, top, bottom, left, right string) error {
 
 	if r.StatusCode != http.StatusOK {
 		return errors.New("failed to update neighbors")
+	}
+
+	return nil
+}
+
+func multitick(unis []string) ([]string, error) {
+	result := []string{}
+	for _, v := range unis {
+		u, err := tick(v)
+		if err != nil {
+			return result, err
+		}
+
+		result = append(result, u)
+	}
+
+	return result, nil
+}
+
+func tick(key string) (string, error) {
+	pth, _ := url.JoinPath("/universe", key)
+	u := &url.URL{
+		Path: pth,
+	}
+
+	req := http.Request{
+		Method: http.MethodPut,
+		URL:    u,
+	}
+	r, err := spinhttp.Send(&req)
+	if err != nil {
+		return "", err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != http.StatusOK {
+		return "", errors.New("failed to advance universes")
+	}
+
+	body, err := io.ReadAll(r.Body)
+	return string(body), nil
+}
+
+func deleteUniverses(unis []string) error {
+	for _, v := range unis {
+		pth, _ := url.JoinPath("/universe", v)
+		u := &url.URL{
+			Path: pth,
+		}
+
+		req := http.Request{
+			Method: http.MethodDelete,
+			URL:    u,
+		}
+		r, err := spinhttp.Send(&req)
+		if err != nil {
+			return err
+		}
+
+		if r.StatusCode != http.StatusOK {
+			return errors.New("failed to delete universes")
+		}
 	}
 
 	return nil
