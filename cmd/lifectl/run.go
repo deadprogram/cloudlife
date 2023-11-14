c5cb64f5c34a9ce4a85e8ed6acdf47feca80fc05
deadprogram <ron@hybridgroup.com>
2023-11-14 13:45:28 +0100

    lifectl: refactor for clarity
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- cmd/lifectl/run.go
+++ cmd/lifectl/run.go
@@ -12,66 +12,73 @@ import (
 	"time"
 )
 
+var tc atomic.Int32
+
 func runMultiverse() {
 	unis, err := getUniverseList()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	list := strings.Split(unis, "\n")
-	var tc atomic.Int32
 
-	go func() {
-		for {
-			var wg sync.WaitGroup
-			for _, v := range list {
-				if v == "" {
-					continue
-				}
-				wg.Add(1)
-				go func(id string, w *sync.WaitGroup) {
-					defer w.Done()
-					_, err := tickUniverse(id)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-					tc.Add(1)
-				}(v, &wg)
-			}
-			wg.Wait()
-		}
-	}()
+	go tickUniverses(unis)
 
 	// show snapshot of data/stats
 	for {
-		data, err := getUniverse(list[0])
+		data, err := getUniverse(unis[0])
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		fmt.Println(data)
 		ticks := tc.Swap(0)
-		fmt.Println(float32(ticks)/float32(len(list)), "multiverses per second")
+		fmt.Println(float32(ticks)/float32(len(unis)), "multiverses per second")
 		fmt.Println(ticks, "universes per second")
+		fmt.Println()
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func getUniverseList() (string, error) {
+func getUniverseList() ([]string, error) {
 	pth, _ := url.JoinPath(host, "multiverse")
 	r, err := http.Get(pth)
 	if err != nil {
-		return "", err
+		return []string{""}, err
 	}
 	defer r.Body.Close()
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		return "", err
+		return []string{""}, err
 	}
 
-	return string(body), nil
+	list := strings.Split(string(body), "\n")
+
+	return list, nil
+}
+
+func tickUniverses(list []string) {
+	for {
+		var wg sync.WaitGroup
+		for _, v := range list {
+			if v == "" {
+				continue
+			}
+			wg.Add(1)
+			go callTickUniverse(v, &wg)
+		}
+		wg.Wait()
+	}
+}
+
+func callTickUniverse(id string, w *sync.WaitGroup) {
+	defer w.Done()
+	_, err := tickUniverse(id)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	tc.Add(1)
 }
 
 func tickUniverse(id string) (string, error) {
